83fe628d40a6f6e791c67d4180b7dd51ee8cda67
Bangstry <tylerwesleyquinn@gmail.com>
2019-09-04 16:39:05 -0400

    stopping here

--- src/App.js
+++ src/App.js
@@ -5,19 +5,25 @@ class App extends React.Component {
   constructor(){
     super();
     this.state= {
+      userName: "Bangstry",
       user: {},
       followers: []
     };
   }
 
+  userChange = (userName) => {
+    this.setState({ userName });
+  }
+
   componentDidMount() {
     console.log("First Render (mounting)");
-    fetch('https://api.github.com/users/bangstry')
+    fetch(`https://api.github.com/users/${this.state.userName}`)
       .then(res => res.json())
       .then(data => this.setState({ user: data }));
-    fetch('https://api.github.com/users/bangstry/followers')
+    fetch(`https://api.github.com/users/${this.state.userName}/followers`)
       .then(res => res.json())
       .then(data => this.setState({ followers: data }));
+        
   }
 
   componentDidUpdate() {
@@ -27,23 +33,58 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Search userChange={this.userChange} />
         <UserCard user={this.state.user} followers={this.state.followers} />
       </div>
     );
   }
 }
 
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: "",
+    };
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.userChange(this.state.search);
+    this.setState({ search: "" });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} >
+        <input type="text"
+               name="search"
+               placeholder="search"
+               value={this.state.search}
+               onChange={this.handleChange}/>
+        <button type="submit">Search for a User</button>
+      </form>
+    );
+  }
+}
+
 function UserCard(props) {
-  return(
+  return (
     <div>
       <h2>{props.user.login}</h2>
       <p>{props.user.location}</p>
       <p>{props.user.url}</p>
       <div>
         {props.followers.map(follower => (
-           <div key={follower.id}>{follower.login}</div>
+          <div key={follower.id}>{follower.login}</div>
         ))}
+      </div>
     </div>
   );
 }
+
 export default App;
